419e99255844ab000ce5037a05a1c2f6a19bb1b4
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-20 19:53:30 -0400

    feat: add calculation functions

---
+++ script.js
@@ -0,0 +1,14 @@
+let leftOperand;
+let operationType;
+let rightOperand;
+let displayValue;
+
+const addCalc = (a, b) => a + b;
+
+const subCalc = (a, b) => a - b;
+
+const multiCalc = (a, b) => a * b;
+
+const diviCalc = (a, b) => a / b;
+
+function operate(operator, num1, num2) {}
