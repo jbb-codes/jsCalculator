4a54ab9f8babe5ee7f10d553c5eee561f90f9917
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-20 21:21:50 -0400

    feat: add listeners for numeric and operation buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let leftOperand;
 let operationType;
 let rightOperand;
-let displayValue;
+let displayValue = "";
 
 const addCalc = (a, b) => a + b;
 
@@ -12,3 +12,39 @@ const multiCalc = (a, b) => a * b;
 const diviCalc = (a, b) => a / b;
 
 function operate(operator, num1, num2) {}
+
+const keys = document.querySelector(".calcKeys");
+
+const display = document.querySelector("#display");
+
+keys.addEventListener("click", (e) => {
+  if (display.textContent !== "0") {
+    displayValue += e.target.textContent;
+    display.textContent = displayValue;
+  } else if (display.textContent === "0") {
+    displayValue = e.target.textContent;
+    display.textContent = e.target.textContent;
+  }
+});
+
+const clearScreen = document.querySelector("#acButton");
+
+clearScreen.addEventListener("click", (e) => {
+  display.textContent = "0";
+  displayValue = "";
+  e.stopPropagation();
+});
+
+const operatorButtons = document.querySelectorAll(".operationButton");
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    operationType = e.target.textContent;
+    console.log(operationType);
+  });
+});
+
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
+  }
+});
