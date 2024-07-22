c00dbffffb6d170d3c66c91862c1c575e7a221f8
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-21 21:36:28 -0400

    fix: number buttons now have seperate listeners from other buttons add decimal and equals button listeners.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let leftOperand;
 let operationType;
 let rightOperand;
 let displayValue = "";
+const maxNumDigits = 7;
 
 const addCalc = (a, b) => a + b;
 
@@ -13,18 +14,32 @@ const diviCalc = (a, b) => a / b;
 
 function operate(operator, num1, num2) {}
 
-const keys = document.querySelector(".calcKeys");
+const equals = document.querySelector("#equalsButton");
+
+equals.addEventListener("click", () => {
+  if (display.textContent !== "0") {
+    displayValue = operate(operationType, leftOperand, rightOperand);
+    display.textContent = displayValue;
+  }
+});
 
 const display = document.querySelector("#display");
 
-keys.addEventListener("click", (e) => {
-  if (display.textContent !== "0") {
-    displayValue += e.target.textContent;
-    display.textContent = displayValue;
-  } else if (display.textContent === "0") {
-    displayValue = e.target.textContent;
-    display.textContent = e.target.textContent;
-  }
+const numbers = document.querySelectorAll(".numberButton");
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (display.textContent !== "0" && displayValue.length <= maxNumDigits) {
+      displayValue += e.target.textContent;
+      display.textContent = displayValue;
+    } else if (
+      display.textContent === "0" &&
+      displayValue.length <= maxNumDigits
+    ) {
+      displayValue = e.target.textContent;
+      display.textContent = displayValue;
+    }
+  });
 });
 
 const clearScreen = document.querySelector("#acButton");
@@ -32,6 +47,7 @@ const clearScreen = document.querySelector("#acButton");
 clearScreen.addEventListener("click", (e) => {
   display.textContent = "0";
   displayValue = "";
+  decimalButton.disabled = false;
   e.stopPropagation();
 });
 
@@ -40,11 +56,14 @@ const operatorButtons = document.querySelectorAll(".operationButton");
 operatorButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     operationType = e.target.textContent;
-    console.log(operationType);
   });
 });
 
-keys.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-  }
+const decimalButton = document.querySelector("#decimalButton");
+
+decimalButton.addEventListener("click", (e) => {
+  displayValue += e.target.textContent;
+  display.textContent = displayValue;
+  decimalButton.disabled = true;
+  e.stopPropagation();
 });
